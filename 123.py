cddede321571ac8493614a5c0b2164abcc0e0eae
JackB <jacky9160@gmail.com>
2021-10-05 00:14:48 +0800

    first test

---
+++ 123.py
@@ -0,0 +1,7 @@
+hight = input('plz key in your hight: ')
+weight = input('plz key in your weight: ')
+name = input('plz key in your name: ')
+print('Hi', name)
+print('your hight is', hight, 'cm')
+print('your weight is', weight, 'kg')
+print('you are so beauty!')
